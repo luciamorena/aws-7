73df03833b448505f96307b49bbfa3637f17a413
Lucia Morena <luciamorena77@gmail.com>
2022-09-02 17:17:46 -0300

    commit 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
+  const [cor, setCor] = useState(null)
+
+  const colorir = () => {
+    const cor = '#' + Math.floor(Math.random()*12345678).toString(16)
+    setCor(cor)
+  }
+
   return (
-    <div className='container'>
-      <h1>SISTEMA</h1>
+    <div className='container' style={{ background: cor }}>
+      <h1 onClick={colorir}>SISTEMA</h1>
     </div>
   )
 }
