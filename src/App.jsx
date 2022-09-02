2998a96c777a9cd93e6e619ba4edd74a5b8ca6de
Lucia Morena <luciamorena77@gmail.com>
2022-09-02 17:31:21 -0300

    commit 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import React, { useState } from 'react'
 
 function App() {
-  const [cor, setCor] = useState(null)
+  const [cor1, setCor1] = useState(null)
+  const [cor2, setCor2] = useState(null)
 
   const colorir = () => {
-    const cor = '#' + Math.floor(Math.random()*12345678).toString(16)
-    setCor(cor)
+    const cor2 = Math.floor(Math.random()*12345678).toString(16)
+    const cor1 = 'f00'
+    setCor1('#' + cor1)
+    setCor2('#' + cor2)
+    console.log(cor1,cor2)
   }
 
   return (
-    <div className='container' style={{ background: cor }}>
+    <div className='container' style={{ background: `linear-gradient(45deg,${cor1},${cor2})` }}>
       <h1 onClick={colorir}>SISTEMA</h1>
     </div>
   )
